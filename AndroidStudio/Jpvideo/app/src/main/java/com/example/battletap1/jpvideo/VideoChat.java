9d42479281a57aab01e785c1ae51d443d6ddb6d9
Pol Lopez <katurro12@gmail.com>
2020-05-27 12:50:51 +0200

    Documentacio Cambiada

--- AndroidStudio/Jpvideo/app/src/main/java/com/example/battletap1/jpvideo/VideoChat.java
+++ AndroidStudio/Jpvideo/app/src/main/java/com/example/battletap1/jpvideo/VideoChat.java
@@ -55,7 +55,7 @@ public class VideoChat extends YouTubeBaseActivity {
                 // Read the input field and push a new instance
                 // of ChatMessage to the Firebase database
                 FirebaseDatabase.getInstance()
-                        .getReference("Missatges")
+                        .getReference("Missatges/" + nameId)
                         .push()
                         .setValue(new ChatMessage(input.getText().toString(),
                                 FirebaseAuth.getInstance()
@@ -103,7 +103,7 @@ public class VideoChat extends YouTubeBaseActivity {
         ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
 
         adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
-                R.layout.message, FirebaseDatabase.getInstance().getReference("Missatges")) {
+                R.layout.message, FirebaseDatabase.getInstance().getReference("Missatges/" + nameId)) {
             @Override
             protected void populateView(View v, ChatMessage model, int position) {
                 // Get references to the views of message.xml
@@ -114,7 +114,7 @@ public class VideoChat extends YouTubeBaseActivity {
                 // Set their text
                 System.out.println(model.getMessageId() + "             " + nameId);
                 System.out.println("==============================================");
-                if (model.getMessageId() == nameId) {
+                if (model.getMessageId().equals(nameId)) {
                     messageText.setText(model.getMessageText());
                     messageUser.setText(model.getMessageUser());
 
